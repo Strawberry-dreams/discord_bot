e686eac53cad8f7b3c72696c3dd008909c7357f8
strawberrydreams <jh08037@naver.com>
2025-05-08 22:18:34 +0900

    Update main.py

--- prefix/main.py
+++ prefix/main.py
@@ -7,6 +7,8 @@ import discord
 import os
 from discord.ext import commands
 from dotenv import load_dotenv
+
+# Import functions registered in a module
 from makeparty_module import register_makeparty_commands
 from forbidfilter_module import register_prohibition_filter
 from eventnotify_module import setup_event_commands
@@ -26,12 +28,16 @@ async def on_ready():
     activity = discord.Activity(type=discord.ActivityType.listening, name="Spotify")
     await bot.change_presence(status=discord.Status.online, activity=activity)
 
-# Register commands in module
+# Import commands registered in module
 register_makeparty_commands(bot)
 register_prohibition_filter(bot)
 setup_event_commands(bot)
 
+# 1. Load token securely from .env file
 load_dotenv(dotenv_path="DISCORD_TOKEN.env")
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot.run(TOKEN)
+
+# 2. Or directly enter the token here (not recommended for real deployments)
+# bot.run("your_bot_token")
